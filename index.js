4f0335969bd183a08afedf978736306cfb95df6d
meetprayas <ag.prayas@gmail.com>
2021-04-20 20:42:28 +0530

    chore: ws ip changed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 
 //initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server: server, path: "/" });
+const wss = new WebSocket.Server({ server: server, path: "/socket" });
 
 // generate a unique ID
 wss.getUniqueID = function () {
